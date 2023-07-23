3b5adfd25e80881b34f98385660e35c89aaced4d
unknown <kamil.mardanov04@yandex.ru>
2023-07-24 00:00:43 +0300

    update: update regex

--- consts/regex.js
+++ consts/regex.js
@@ -1,5 +1,5 @@
 const linkExp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/i;
-const ruNameExp = /^[a-zA-Zа-яА-Я0-9\s+,:;=?'"<>.^*()%!&-]+$/;
-const enNameExp = /^[a-zA-Zа-яА-Я0-9\s+,:;=?'"<>.^*()%!&-]+$/;
+const ruNameExp = /^[a-zA-Zа-яА-Я0-9\s+,:;=?'"<>.^*()%!&-«»]+$/;
+const enNameExp = /^[a-zA-Zа-яА-Я0-9\s+,:;=?'"<>.^*()%!&-«»]+$/;
 
 module.exports = { linkExp, ruNameExp, enNameExp };
